b773bff72ac01e18eb3a4c84310acb1713d9446e
Teklehaimanot <teklehaimanotbelete@gmail.com>
2025-05-05 17:30:44 +0300

    get rumours api is created

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const rummersRouter = require("./routes/rumorsRoute");
 const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send(" you are on the home page");
 });
+app.use("/rumors", rummersRouter);
 
 app.listen(5000, () => {
   console.log("The servier is running on port 5000");
---
+++ routes/rumorsRoute.js
@@ -0,0 +1,8 @@
+const express = require("express");
+// const { getAllRumours } = require("../constrollers/rumorsController");
+rummersController = require("../constrollers/rumorsController");
+const rummersRouter = express.Router();
+
+rummersRouter.get("/", rummersController.getAllRumours);
+
+module.exports = rummersRouter;
